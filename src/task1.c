76940587f421aac56e41b2d5e761b76d6a2b55bd
Gergely Lendvay <28988586+gegelendvay@users.noreply.github.com>
2026-03-10 15:49:29 +0100

    feat: Implement different loop order

--- src/task1.c
+++ src/task1.c
@@ -3,13 +3,27 @@
 #include <time.h>
 #include <omp.h>
 
+// Explore more parallelization and collapse options
+
 void multiply_matrix(double *A, double *B, double *C, int N) {
-    #pragma omp parallel for
+    #pragma omp parallel for schedule (static) 
     for(int i=0; i<N; i++) {
         for(int j=0; j<N; j++) {
             C[i*N + j] = 0.0;
             for(int k=0; k<N; k++) {
                 C[i*N + j] += A[i*N + k] * B[k*N + j];
+            }
+        }
+    }
+}
+
+void multiply_matrix_ikj(double *A, double *B, double *C, int N) {
+    #pragma omp parallel for schedule (static)
+    for(int i=0; i<N; i++) {
+        for(int k=0; k<N; k++) {
+            double a = A[i*N + k];
+            for(int j=0; j<N; j++) {
+                C[i*N + j] += a * B[k*N + j];
             }
         }
     }
