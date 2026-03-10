000a9b8f6e3614b50de4bccd076758363bdbb4d4
Gergely Lendvay <28988586+gegelendvay@users.noreply.github.com>
2026-03-10 15:41:35 +0100

    feat: Finish task 1

--- src/task1.c
+++ src/task1.c
@@ -4,9 +4,54 @@
 #include <omp.h>
 
 void multiply_matrix(double *A, double *B, double *C, int N) {
-    
+    #pragma omp parallel for
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            C[i*N + j] = 0.0;
+            for(int k=0; k<N; k++) {
+                C[i*N + j] += A[i*N + k] * B[k*N + j];
+            }
+        }
+    }
+}
+
+double checksum(double *C, int N) {
+    double sum = 0.0;
+    for(int i=0; i<N*N; i++) {
+        sum += C[i];
+    }
+    return sum;
 }
 
 int main() {
-    return 0
+    int N = 1024;
+    double *A = (double*)malloc(N*N*sizeof(double));
+    double *B = (double*)malloc(N*N*sizeof(double));
+    double *C = (double*)malloc(N*N*sizeof(double));
+
+    srand(time(NULL));
+
+    for(int i=0; i<N*N; i++) {
+        A[i] = (double)rand() / RAND_MAX;
+        B[i] = (double)rand() / RAND_MAX;
+    }
+
+    omp_set_num_threads(4);
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    multiply_matrix(A, B, C, N);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Time: %f seconds\n", time);
+
+    double checksum_C = checksum(C, N);
+    printf("Checksum: %f\n", checksum_C);
+
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
 }
