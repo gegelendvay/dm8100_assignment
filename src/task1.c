7053e1254b57213a8c7b5c70dffc1c9ff67d9056
Gergely Lendvay <28988586+gegelendvay@users.noreply.github.com>
2026-03-10 07:43:51 +0100

    feat: Finish task 0

---
+++ src/task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+void multiply_matrix(double *A, double *B, double *C, int N) {
+    
+}
+
+int main() {
+    return 0
+}
