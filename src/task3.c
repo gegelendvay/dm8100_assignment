9406a51133292747405ef341aaaa082658ddf619
ana <napirosi@gmail.com>
2026-04-06 11:32:16 +0200

    Primer try

---
+++ src/task3.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+__global__ void multiply_matrix(double *A, double *B, double *C, int N) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            C[i*N + j] = 0.0;
+            for(int k=0; k<N; k++) {
+                //C[i*N + j] += A[i*N + k] * B[k*N + j];
+                C[i*N + j] += A[i*N + k] * B[k*N + j];
+            }
+        }
+    }
+}
+
+double checksum(double *C, int N) {
+    double sum = 0.0;
+    for(int i=0; i<N*N; i++) {
+        sum += C[i];
+    }
+    return sum;
+}
+
+int main() {
+    int N = 1024;
+    double *A = (double*)malloc(N*N*sizeof(double));
+    double *B = (double*)malloc(N*N*sizeof(double));
+    double *C = (double*)malloc(N*N*sizeof(double));
+
+    srand(time(NULL));
+
+    for(int i=0; i<N*N; i++) {
+        A[i] = (double)rand() / RAND_MAX;
+        B[i] = (double)rand() / RAND_MAX;
+    }
+
+    double *d_A 
+    double *d_B
+    double *d_C
+
+    cudaMalloc((void**)&d_A, N*N*sizeof(double));
+    cudaMalloc((void**)&d_B, N*N*sizeof(double));
+    cudaMalloc((void**)&d_C, N*N*sizeof(double));
+
+    cudaMemcpy(d_A, A, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_C, C, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+    //cada thread encuentra uno de los valores de la matriz resultante 
+    //probar cual es la mejor forma de repartilo al ejecutarlo 
+    grid_size =
+    block_size = 
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    multiply_matrix<<<grid_size,block_size>>>(A, B, C, N);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    cudaMemcpy(C, d_C, sizeof(float)*N*N, cudaMemcpyDeviceToHost);
+
+    double time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Time: %f seconds\n", time);
+
+    double checksum_C = checksum(C, N);
+    printf("Checksum: %f\n", checksum_C);
+
+    free(A);
+    free(B);
+    free(C);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    return 0;
+}
